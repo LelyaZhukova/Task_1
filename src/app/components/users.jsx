b6495f6afa49a01d0c0102757872687a5c4800d8
Olga <soul666@mail.ru>
2022-12-26 15:51:19 +0300

    new

--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { paginate } from "../utils/paginate";
+import Pagination from "./pagination";
 import User from "./user";
-const Users = ({ users, ...rest }) => {
+const Users = ({ users: allUsers, ...rest }) => {
+    const count = allUsers.length;
+    const pageSize = 4;
+    const [currentPage, setCurrentPage] = useState(1);
+    const handlePageChange = (pageIndex) => {
+        console.log("page: ", pageIndex);
+        setCurrentPage(pageIndex);
+    };
+
+    const userCrop = paginate(allUsers, currentPage, pageSize);
     return (
         <>
-            {users.length > 0 && (
+            {count > 0 && (
                 <table className="table">
                     <thead>
                         <tr>
                             <th scope="col">Имя</th>
                             <th scope="col">Качества</th>
-                            <th scope="col">Провфессия</th>
+                            <th scope="col">Профессия</th>
                             <th scope="col">Встретился, раз</th>
                             <th scope="col">Оценка</th>
                             <th scope="col">Избранное</th>
@@ -17,14 +29,23 @@ const Users = ({ users, ...rest }) => {
                         </tr>
                     </thead>
                     <tbody>
-                        {users.map((user) => (
-                            <User key={user._id} {...rest} {...user} />
+                        {userCrop.map((user) => (
+                            <User {...rest} {...user} key={user._id} />
                         ))}
                     </tbody>
                 </table>
             )}
+            <Pagination
+                itemsCount={count}
+                pageSize={pageSize}
+                currentPage={currentPage}
+                onPageChange={handlePageChange}
+            />
         </>
     );
+};
+Users.propTypes = {
+    users: PropTypes.array
 };
 
 export default Users;
